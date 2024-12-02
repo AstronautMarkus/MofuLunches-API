0fca029f0779ffa533ab78625463eba459ae5813
Marcos Reyes <astronautmarkus@gmail.com>
2024-12-02 17:02:09 -0300

    Added WebSocket for pedido status

--- mofulunches-api/pedidos_service/app.py
+++ mofulunches-api/pedidos_service/app.py
@@ -1,26 +1,28 @@
 import os
 from flask import Flask
 from flask_jwt_extended import JWTManager
-from dotenv import load_dotenv 
+from flask_socketio import SocketIO
+from dotenv import load_dotenv
 from blueprints.pedidos import pedidos_bp
 from config import Config
 
-
 load_dotenv()
+
+# SocketIO
+socketio = SocketIO(cors_allowed_origins="*")
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
     
-    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "default_jwt_password")  
-    jwt = JWTManager(app) 
-
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "default_jwt_password")
+    jwt = JWTManager(app)
+    
     app.register_blueprint(pedidos_bp)
-    
 
     return app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True, port=5002)
+    socketio.init_app(app)  # Start SocketIO
+    socketio.run(app, debug=True, port=5002)
